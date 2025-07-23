bef758e9fed7f2de37ac4448af435cd1602578d4
corgifist <sxuare.science@gmail.com>
2025-07-23 17:16:47 +0300

    analytics: trying to fix not working analytics

--- src/app/analytics.tsx
+++ src/app/analytics.tsx
@@ -3,10 +3,14 @@ import { ReactNode } from "react";
 // Powered by Umami (https://umami.is/)
 export default function Analytics(): ReactNode {
     if (process.env.NODE_ENV == "development") {
-        console.log("Analytics is disabled in development mode");
+        console.log("Analytics is disabled in development environment");
+        return (<></>);
+    }
+    if (process.env.ENABLE_ANALYTICS != "true") {
+        console.log("Analytics is disabled (ENABLE_ANALYTICS=false)");
         return (<></>);
     }
     return (
         <script defer src={process.env.ANALYTICS_SCRIPT} data-website-id={process.env.ANALYTICS_WEBSITE_ID}/>
-    )
+    );
 }
