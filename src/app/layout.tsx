bf36d86a6793b8452e77075e7ddda6d6f4630f30
corgifist <sxuare.science@gmail.com>
2025-07-25 02:46:35 +0300

    trying to disable zooming in on mobile (second attempt)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,8 @@ export default async function RootLayout({
         <html lang="en" suppressHydrationWarning>
             <head>
                 <meta name="apple-mobile-web-app-title" content="ClipFusion" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+                <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport"/>
+                <meta name="apple-mobile-web-app-capable" content="yes"/>
                 <meta name="HandheldFriendly" content="true" />
                 <Analytics/>
             </head>
