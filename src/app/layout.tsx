779351db178498a568391e790fa5d6573487f9d6
corgifist <sxuare.science@gmail.com>
2025-07-28 13:59:48 +0300

    replaced global bottom margin with global top margin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default async function RootLayout({
                 <ThemeProvider>
                     <SidebarProvider>
                         <Dashboard/>
-                        <main className="relative mb-safe">
+                        <main className="relative mt-safe">
                             <PersistenceProvider>
                                 {children}
                             </PersistenceProvider>
