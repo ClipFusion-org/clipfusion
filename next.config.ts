70aa3992f814ef2434cf22a445c391e0f4f1b4fc
corgifist <sxuare.science@gmail.com>
2025-07-28 19:18:49 +0300

    trying to get git commit hash to work

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+    generateBuildId: () => {
+        if (process.env.NODE_ENV === "development") return "development server";
+        if (!process.env.GIT_COMMIT) return "git commit hash is unavailable";
+        const hash = process.env.GIT_COMMIT;
+        if (!hash || hash.trim().length == 0) return "empty git commit hash";
+        return hash;
+    }
 };
 
 export default nextConfig;
