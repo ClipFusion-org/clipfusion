e33d04609d298129170b3829e2dfbf81e17c425e
corgifist <sxuare.science@gmail.com>
2025-08-02 03:44:59 +0300

    trying to fix overflowing in the editor page

--- src/app/(editor)/editor/[uuid]/layout.tsx
+++ src/app/(editor)/editor/[uuid]/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
     children: ReactNode
 }>) {
     return (
-        <main className="relative w-screen h-screen isolate overscroll-none">
+        <main className="relative w-screen h-screen isolate overscroll-none overflow-hidden mb-safe">
             <PersistenceProvider>
                 {children}
             </PersistenceProvider>
